93fd931923751fa04eb2cf0882852de3fd71153b
fabioguelfi <fabioguelfiunix@gmail.com>
2017-11-15 09:21:01 -0200

    inicio if para avaliacao de seguranca / quant minima ok

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ $(function(){
     $('#senha').bind('keyup',function () {
 
        var txt = $(this).val();
-        $('#forca').html(txt);
+       var forca = 0;
+
+        // letras
+        // numeros
+        // caracteres especiais
+        // numero minimo
+
+        if(txt.length > 6){
+            forca += 25;
+        }
+        $('#forca').html(forca)
 
     });
 });
