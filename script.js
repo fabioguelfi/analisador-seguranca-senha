fb910800f987692c1b1fc0d1df59a6cb8a4c32a4
Fabio Guelfi <fabioguelfiunix@gmail.com>
2017-11-17 13:29:23 -0200

    final validacoes via regex

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-$(function(){
-    $('#senha').bind('keyup',function () {
+$(function () {
+    $('#senha').bind('keyup', function () {
 
-       var txt = $(this).val();
-       var forca = 0;
+        var txt = $(this).val();
+        var forca = 0;
 
         // letras
         // numeros
         // caracteres especiais
         // numero minimo
 
-        if(txt.length > 6){
+        if (txt.length >= 7) {
             forca += 25;
         }
-        $('#forca').html(forca)
+
+        var reg = new RegExp(/[a-z]/i);
+        if(reg.test(txt)){
+            forca += 25;
+        }
+
+        var reg = new RegExp(/[0-9]/i);
+        if(reg.test(txt)){
+            forca += 25;
+        }
+
+        var reg = new RegExp(/[^a-z0-9]/i);
+        if(reg.test(txt)){
+            forca += 25;
+        }
+
+        $('#forca').html('Força: ' + forca);
 
     });
 });
